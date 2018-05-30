ccdeca5751ff9a5977f73e33b04dd33e0113456c
Bryan Sebastian <bryanrsebastian@gmail.com>
2018-05-30 14:50:34 +0800

    Remove the prefix billing_ on the validation key. Update also the shipping address.

--- validation-saving.php
+++ validation-saving.php
@@ -10,25 +10,22 @@
 */
 function wooc_validate_extra_register_fields( $username, $email, $validation_errors ) {
 	if ( isset( $_POST['first_name'] ) && empty( $_POST['first_name'] ) )
-		$validation_errors->add( 'billing_first_name_error', __( '<strong>Error</strong>: First name is required.', 'woocommerce' ) );
+		$validation_errors->add( 'first_name_error', __( '<strong>Error</strong>: First name is required.', 'woocommerce' ) );
 
 	if ( isset( $_POST['last_name'] ) && empty( $_POST['last_name'] ) )
-		$validation_errors->add( 'billing_last_name_error', __( '<strong>Error</strong>: Last name is required.', 'woocommerce' ) );
-	
-	if ( isset( $_POST['contact_number'] ) && empty( $_POST['contact_number'] ) )
-		$validation_errors->add( 'billing_phone_error', __( '<strong>Error</strong>: Contact number is required.', 'woocommerce' ) );
+		$validation_errors->add( 'last_name_error', __( '<strong>Error</strong>: Last name is required.', 'woocommerce' ) );
 	
 	if ( isset( $_POST['address'] ) && empty( $_POST['address'] ) )
-		$validation_errors->add( 'billing_address_1_error', __( '<strong>Error</strong>: Address is required.', 'woocommerce' ) );
+		$validation_errors->add( 'address_1_error', __( '<strong>Error</strong>: Address is required.', 'woocommerce' ) );
 	
 	if ( isset( $_POST['city'] ) && empty( $_POST['city'] ) )
-		$validation_errors->add( 'billing_city_error', __( '<strong>Error</strong>: City is required.', 'woocommerce' ) );
+		$validation_errors->add( 'city_error', __( '<strong>Error</strong>: City is required.', 'woocommerce' ) );
 	
 	if ( isset( $_POST['zip_code'] ) && empty( $_POST['zip_code'] ) )
-		$validation_errors->add( 'billing_zip_code_error', __( '<strong>Error</strong>: Postal / Zip code is required.', 'woocommerce' ) );
+		$validation_errors->add( 'zip_code_error', __( '<strong>Error</strong>: Postal / Zip code is required.', 'woocommerce' ) );
 	
 	if ( isset( $_POST['country'] ) && empty( $_POST['country'] ) )
-		$validation_errors->add( 'billing_country_error', __( '<strong>Error</strong>: Country is required.', 'woocommerce' ) );
+		$validation_errors->add( 'country_error', __( '<strong>Error</strong>: Country is required.', 'woocommerce' ) );
 
 	if ( isset( $_POST['retype_password'] ) && empty( $_POST['retype_password'] ) )
 		$validation_errors->add( 'retype_password_error', __( '<strong>Error</strong>: Re-Type password is required.', 'woocommerce' ) );
@@ -54,6 +51,9 @@ function wooc_save_extra_register_fields( $customer_id ) {
 
 		// WooCommerce billing first name.
 		update_user_meta( $customer_id, 'billing_first_name', sanitize_text_field( $_POST['first_name'] ) );
+
+		// WooCommerce shipping first name.
+		update_user_meta( $customer_id, 'shipping_first_name', sanitize_text_field( $_POST['first_name'] ) );
 	}
 
 	if ( isset( $_POST['last_name'] ) ) {
@@ -62,6 +62,9 @@ function wooc_save_extra_register_fields( $customer_id ) {
 
 		// WooCommerce billing last name.
 		update_user_meta( $customer_id, 'billing_last_name', sanitize_text_field( $_POST['last_name'] ) );
+
+		// WooCommerce shipping last name.
+		update_user_meta( $customer_id, 'shipping_last_name', sanitize_text_field( $_POST['last_name'] ) );
 	}
 
 	if ( isset( $_POST['contact_number'] ) ) {
@@ -72,21 +75,33 @@ function wooc_save_extra_register_fields( $customer_id ) {
 	if ( isset( $_POST['address'] ) ) {
 		// WooCommerce billing address line 1
 		update_user_meta( $customer_id, 'billing_address_1', sanitize_text_field( $_POST['address'] ) );
+
+		// WooCommerce shipping address line 1
+		update_user_meta( $customer_id, 'shipping_address_1', sanitize_text_field( $_POST['address'] ) );
 	}
 
 	if ( isset( $_POST['city'] ) ) {
 		// WooCommerce billing city
 		update_user_meta( $customer_id, 'billing_city', sanitize_text_field( $_POST['city'] ) );
+
+		// WooCommerce shipping city
+		update_user_meta( $customer_id, 'shipping_city', sanitize_text_field( $_POST['city'] ) );
 	}
 
 	if ( isset( $_POST['zip_code'] ) ) {
 		// WooCommerce billing postcode
 		update_user_meta( $customer_id, 'billing_postcode', sanitize_text_field( $_POST['zip_code'] ) );
+
+		// WooCommerce shipping postcode
+		update_user_meta( $customer_id, 'shipping_postcode', sanitize_text_field( $_POST['zip_code'] ) );
 	}
 
 	if ( isset( $_POST['country'] ) ) {
 		// WooCommerce billing country
 		update_user_meta( $customer_id, 'billing_country', sanitize_text_field( $_POST['country'] ) );
+
+		// WooCommerce shipping country
+		update_user_meta( $customer_id, 'shipping_country', sanitize_text_field( $_POST['country'] ) );
 	}
 }
 add_action( 'woocommerce_created_customer', 'wooc_save_extra_register_fields' );
